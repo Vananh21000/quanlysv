0b36b61408fd8ed1c9764d9b80f485d19408c958
Vân Anh <79819511+Vananh21000@users.noreply.github.com>
2021-09-25 23:21:21 +0700

    Add files via upload

---
+++ QLSinhVien/src/Interface/tblDSLop.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Van Anh
+ */
+class tblDSLop {
+
+    static DefaultTableModel getModel() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static int getSelectedRow() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
